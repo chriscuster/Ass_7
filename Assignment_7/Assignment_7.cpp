5f24700c813e0ab0fce6457cee21b61669fc6761
Chris Custer <chris@custerfamily.com>
2019-10-30 17:38:52 -0700

    first

---
+++ Assignment_7/Assignment_7.cpp
@@ -0,0 +1,176 @@
+/*
+Christopher Custer
+Assignment 7
+Description: Use loops to create various different arrows and output it to a file.
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+stringstream output;
+
+
+//function for creating a box from user input with designated chars
+void printZero(int size, char letter)
+{
+	// output header info of file
+	output << "Symbol: Zero" << " Size: " << size << " Character: " << letter;
+	if (size < 5)
+	{
+		size = 5;
+		output << "Zero must have at least a size of 5.  Shape created with size: 5" << endl;
+	}
+	else
+	{
+		output << endl;
+	}
+	stringstream outputChars;
+
+	// keeps the count of which column you are on for output
+	int headerinfo = 0;
+	for (int length = 0; length < size; length++)
+	{
+		headerinfo = headerinfo + 1;
+		if (headerinfo > 9)
+		{
+			headerinfo = 0;
+		}
+		output << headerinfo;
+
+		// if first or last line, fill in entire row
+		if (length == 0 || length == size - 1)
+		{
+			for (int height = 0; height < size; height++)
+			{
+				outputChars << letter;
+			}
+		}
+		else
+		{
+			for (int height = 0; height < size; height++)
+			{
+				if (height == size - length - 1 || height == 0 || height == size - 1)
+				{
+					outputChars << letter;
+				}
+				else
+				{
+					outputChars << " ";
+				}
+			}
+		}
+		outputChars << endl;
+	}
+	output << endl << endl << outputChars.str();
+}
+
+void printUpArrow(int size, char letter)
+{
+	int height = 0;
+	stringstream outputChars;
+
+	//makes the size even if its odd
+	if (size %2 != 0)
+	{
+		size++;
+	}
+	while ( height < size)
+	{
+		//if the height is the upper half create the arrow
+		if (height <= size / 2)
+
+		//if height is the bottom half create the base shape
+		if (height <= size - (size / 2))
+		{
+			outputChars << letter;
+			outputChars << letter;
+			//outputChars << "";
+		}
+		else
+		{
+			outputChars << " ";
+		}
+		height++;
+	}
+	output << endl << endl << outputChars.str();
+}
+
+void printRightArrow(int size, char letter)
+{
+	//makes the size even if its odd
+	if (size % 2 != 0)
+	{
+		size++;
+	}
+
+
+}
+
+
+int main(int argc, char* argv[])
+{
+
+	int size = 0;
+	char letter = ' ';
+	string shape = "";
+	string filename = "data.txt";
+	ifstream fin;
+
+	//statement to check if the file opened or not
+	do
+	{
+		// todo: remove for final project 
+		if (argc == 1)
+		{
+			cout << "Open file: ";
+			cin >> filename;
+		}
+		fin.open(filename.c_str());
+		if (!fin.is_open())
+		{
+			cout << "Could not open " << filename << endl;
+		}
+	} while (!fin.is_open());
+
+	while (!fin.eof())
+	{
+		fin >> shape;
+		fin >> size;
+		fin >> letter;
+		// protects against empty lines at end of file
+		if (!fin.good())
+		{
+			break;
+		}
+		string filename = shape + "_" + to_string(size) + ".txt";
+
+		ofstream fout(filename);
+		if (shape == "zero")
+		{
+			printZero(size, letter);
+			fout << output.str();
+		}
+
+		if (shape == "upArrow")
+		{
+			printUpArrow(size, letter);
+			fout << output.str();
+		}
+
+		if (shape == "rightArrow")
+		{
+			cout << endl;
+		}
+		fout.close();
+		output.str("");
+		
+		//cout << "Shape Created: " << shape << endl;
+
+	}
+
+	fin.close();
+}
